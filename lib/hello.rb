22e5f02bdb4101a8c6f94b7e137c7e096ab2c7c4
Teinelle Charles <teinelledcharles@gmail.com>
2020-04-23 02:48:39 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -12,3 +12,14 @@ ddef hello_t(array)
     puts "Hey! No block was given!"
   end
 end
+
+def hello_t(array)
+  i = 0
+ 
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+ 
+  array
+end
